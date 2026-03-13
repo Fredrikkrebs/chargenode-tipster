4a5272869fad78b9de08d60d2da1e75c366aba7f
Fredrikkrebs <fredrik.krebs@chargenode.eu>
2026-03-13 15:24:55 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
   SUPABASE_URL: "https://jviipglfhcvnyploohnf.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2aWlwZ2xmaGN2bnlwbG9vaG5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MDkxMTAsImV4cCI6MjA4ODk4NTExMH0.otWd-moKoMund9Zq6jUEski3xZ1f_RTYTukvCZQbFo8",
-  ADMIN_PASS: "byt-mig",
+  ADMIN_PASS: "tipsadmin",
   SCORING: { EXACT: 3, OUTCOME: 1 },
   FUNCTIONS_URL: "" // Slack kommer här sen
 };
